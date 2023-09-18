dfb0245bb85312c119b70fa00edebcb8bdef6f56
Tim Mitra <tim@it-guy.com>
2023-09-17 20:27:18 -0400

    Added a second Networking API for sending data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,9 +9,17 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void)
 }
 
 extension URLSession: NetworkSession {
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         let task = dataTask(with: url) { data, _, error in
             completionHandler(data, error)
@@ -43,6 +51,27 @@ extension RazeCore {
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     // pass the result into the completionHander and let dev to the rest
                     completionHandler(result)
+                }
+            }
+            
+            /// Call to live internet to send data to a specific location
+            /// - Warning: Make sure the URL can accept a POST route
+            /// - Parameters:
+            ///   - url: The location you want to send data to
+            ///   - body: The object you wsih to send over the network
+            ///   - completionHandler: Returns a result which signifies the status of the request
+            public func sendData<I: Codable>(to url: URL, body: I, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                do {
+                    let httpBody = try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    request.httpMethod = "POST"
+                    session.post(with: request) { data, error in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completionHandler(result)
+                    }
+                } catch {
+                    return completionHandler(.failure(error))
                 }
             }
         }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -9,12 +9,22 @@ import XCTest
 @testable import RazeCore
 
 class NetworkSessionMock: NetworkSession {
+    
     var data: Data?
     var error: Error?
     
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         completionHandler(data, error)
     }
+
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
 }
 
 final class RazeNetworkingTests: XCTestCase {
@@ -41,7 +51,31 @@ final class RazeNetworkingTests: XCTestCase {
         wait(for: [expectation])
     }
     
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "Tim")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Sent data")
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+                break
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+    
     static var allTests = [
-        ("testLoadDataCall", testLoadDataCall)
+        ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall)
     ]
 }
